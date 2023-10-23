82f20f9b485d16954a5f1769b76363436a16cd91
GuidoLorenzetti <guidobruera@gmail.com>
2023-10-23 20:05:39 -0300

    scrap_page terminado

--- obtener_noticias.py
+++ obtener_noticias.py
@@ -4,11 +4,38 @@ import requests
 import pandas as pd
 import numpy as np
 import re
+import requests
+from bs4 import BeautifulSoup
+
 
 noticias = pd.read_csv('noticias.csv', sep=';')
-def scrap_page(pagina):
-   
-    
+noticias = noticias.iloc[0:0]
 
+def scrap_page(categoria):
+    noticias = []
+    # URL de la página que deseas raspar
+    url = f"https://www.infobae.com/{categoria}/"
 
-    noticias.to_csv('noticias.csv', sep=';', index=False)
+    # Realizar una solicitud GET a la URL
+    response = requests.get(url)
+
+    # Verificar si la solicitud fue exitosa (código de estado 200)
+    if response.status_code == 200:
+        # Analizar el contenido HTML de la página con BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Encontrar todos los enlaces en la página
+        divs = soup.find_all('div', {"class":'story-card-info'})
+        links = [x.find_all('a', href=True) for x in divs]
+        links1 = [item for sublist in links for item in sublist]
+        # Filtrar los enlaces que empiezan por "{cateogoria}/" y no están en elementos de imagen
+        url_links = [link['href'] for link in links1 if link['href'].startswith(f'/{categoria}/')]
+
+        # Imprimir los enlaces
+        for link in url_links:
+            if f"https://www.infobae.com/{link}" not in noticias:
+                noticias.append(f"https://www.infobae.com/{link}")
+    else:
+        print(f"No se pudo acceder a la página. Código de estado: {response.status_code}")
+
+    return noticias
