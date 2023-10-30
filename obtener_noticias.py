e8b5a8e1396ba9eaa23031e3e7cedb6b24e800a8
GuidoLorenzetti <guidobruera@gmail.com>
2023-10-30 19:13:20 -0300

    funcion cargar_csv terminada

--- obtener_noticias.py
+++ obtener_noticias.py
@@ -5,10 +5,6 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-
-noticias = pd.read_csv('noticias.csv', sep=';')
-noticias = noticias.iloc[0:0]
-
 def scrap_page(categoria):
     noticias = []
     # URL de la página que deseas raspar
@@ -45,3 +41,65 @@ def scrap_page(categoria):
         print(f"No se pudo acceder a la página. Código de estado: {response.status_code}")
 
     return noticias
+
+def agregar_text_title(df, url, titulo, texto, categoria):
+    # Crear una nueva fila como un diccionario
+    nueva_fila = {'url': url,'titulo': titulo, 'texto': texto, 'categoria': categoria}
+
+    # Usar el método append para agregar la nueva fila al DataFrame
+    df = pd.concat([df, pd.DataFrame([nueva_fila])], ignore_index=True)
+
+    return df
+
+def title_text(df, url, categoria):
+    #Acá voy a tener una lista de urls, la recorro y hago esto para cada noticia
+    # URL de la página a la que deseas hacer web scraping
+    #Aca lo cargo a la fila del csv en df["url"]
+
+
+    # Realiza una solicitud GET a la página
+    response = requests.get(url)
+
+    # Verifica si la solicitud fue exitosa (código 200)
+    if response.status_code == 200:
+        # Parsea el contenido HTML de la página usando BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Encuentra el elemento HTML que contiene el título
+        # Ajusta el selector CSS según la estructura de la página
+        title_element = soup.find('h1', class_='article-headline')
+
+        # Extrae y muestra el texto del título
+        title_text = title_element.text.strip()
+        #print(f'Título: {title_text}')
+
+        # Encuentra los elementos HTML que son párrafos
+        # En este caso, simplemente buscamos todos los elementos <p> en la página
+        paragraph_elements = soup.find_all('p', class_='paragraph')
+
+        texto = ""
+        # Itera a través de los elementos y muestra el texto de los párrafos
+        for paragraph_element in paragraph_elements:
+            # Extrae y muestra el texto del párrafo
+            paragraph_text = paragraph_element.text.strip()
+            texto = texto + paragraph_text
+
+        df = agregar_text_title(df, url, title_text, texto, categoria)
+
+    else:
+        print(f'Error al obtener la página. Código de estado: {response.status_code}')
+
+    return df
+
+def cargar_csv():
+    df = pd.DataFrame(columns=['url', 'titulo', 'texto', 'categoria'])
+    categoria = ["economia", "deportes", "salud", "tecno"]
+
+    for categ in categoria:
+        noticias = scrap_page(categ)
+        for url in noticias:
+            df = title_text(df, url, categ)
+
+    df.to_csv('noticias.csv', sep=';', index=False) 
+
+    return df
